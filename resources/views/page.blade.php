13e93e01fb2dd681ffde839b94ca9a07ea68b037
Jay1991 <juliusfsmziray@gmail.com>
2016-09-13 13:44:15 +0300

    scripts for sidebar desktop

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -37,7 +37,7 @@
   @endforeach
   <div class="col-md-3 col-md-offset-1 hidden-sm hidden-xs text-center">
     <nav data-spy="affix" data-offset-top="0" data-offset-bottom="100" style="background-color:#42A4DB; height:480px; width:300px; position:fixed;">
-      <h5>YOUR ADS HERE</h5>
+      <script data-cfasync='false' type='text/javascript' src='//clksite.com/adServe/banners?tid=163359_288755_5'></script>
     </nav>
   </div>
 
