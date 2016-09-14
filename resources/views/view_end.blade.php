470d5065116d18687cba04a3389992a0eaa0d75a
Jay1991 <juliusfsmziray@gmail.com>
2016-09-14 13:00:25 +0300

    add forgotten fbroot to viewend body

--- resources/views/view_end.blade.php
+++ resources/views/view_end.blade.php
@@ -9,6 +9,20 @@
 <meta property="og:description"   content="{{ $posts->summary }}" />
 <meta property="og:image"         content="{{ $posts->media }}" />
 @endsection
+@section('fbroot')
+<!-- facebook sdk -->
+<div id="fb-root"></div>
+<script>
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.async = true;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+</script>
+@endsection
 
 @section('content')
 <div class="container top-container">
